2b2c17822a7a56def2f53511037f3dedb5145256
Chris Stead <cmstead@gmail.com>
2020-07-26 23:16:30 -0700

    Finished read functionality

--- modules/runtime/EnvironmentTable.js
+++ modules/runtime/EnvironmentTable.js
@@ -17,24 +17,45 @@ const EnvironmentTable = (function () {
         },
 
         initialize: function (key, value) {
+            const originalType = typeof value;
+            let varValue = value;
+
             Object.defineProperty(this.identifiers, key, {
-                value: value
+                get: function () {
+                    return varValue;
+                },
+
+                set: function (newValue) {
+                    if(typeof newValue !== originalType) {
+                        throw new Error(`Cannot assign '${newValue}' to '${key}'. New value type must be '${originalType}'`);
+                    }
+
+                    varValue = newValue;
+                }
             });
         },
         
         read: function (key) {
-            return this.identifiers[key];
+            if(typeof this.identifiers[key] !== 'undefined') {
+                return this.identifiers[key];
+            } else if(this.parent !== null) {
+                return this.parent.read(key);
+            } else {
+                throw new Error(`Variable '${key}' has not been created.`);
+            }
         },
         
         update: function (key, value) {
             this.identifiers[key] = value;
         },
         
-        new: function () { }
+        new: function () {
+            return EnvironmentTable.new(this);
+        }
     };
 
-    EnvironmentTable.new = function () {
-        return new EnvironmentTable();
+    EnvironmentTable.new = function (parentScope) {
+        return new EnvironmentTable(parentScope);
     }
 
     return EnvironmentTable;
--- test/runtime/EnvironmentTable.test.js
+++ test/runtime/EnvironmentTable.test.js
@@ -1,4 +1,3 @@
-const mocha = require('mocha');
 const { assert } = require('chai');
 
 const EnvironmentTable = require('../../modules/runtime/EnvironmentTable');
@@ -39,6 +38,53 @@ describe('Environment Table', function () {
             localScope.initialize(varName, expectedValue);
 
             assert.equal(localScope.read(varName), expectedValue);
+        });
+
+        it('updates normally when original type is adhered to', function () {
+            const localScope = EnvironmentTable.new();
+
+            const varName = 'testVar';
+            const expectedValue = 'This is a test value';
+
+            localScope.initialize(varName, expectedValue);
+
+            assert.doesNotThrow(() => localScope.update(varName, 'this is a test'));
+        });
+
+        it('throws on update when new value does not match old type', function () {
+            const localScope = EnvironmentTable.new();
+
+            const varName = 'testVar';
+            const expectedValue = 'This is a test value';
+            const newValue = 5;
+
+            localScope.initialize(varName, expectedValue);
+
+            assert.throws(() => localScope.update(varName, newValue), `Cannot assign '${newValue}' to '${varName}'. New value type must be '${typeof expectedValue}'`);
+        });
+    });
+
+    describe('read', function () {
+        it('reads through the environment tree to the nearest scoped variable requested', function () {
+            const parentScope = EnvironmentTable.new();
+            const localScope = parentScope.new();
+
+            const varName = 'testVar';
+            const testValue = 'This is a test value';
+
+            parentScope.initialize(varName, testValue);
+
+            assert.equal(localScope.read(varName), testValue);
+        });
+
+        it('throws an error if the variable is not defined', function () {
+            const parentScope = EnvironmentTable.new();
+            const localScope = parentScope.new();
+
+            const varName = 'testVar';
+            const testValue = 'This is a test value';
+
+            assert.throws(() => localScope.read(varName), `Variable '${varName}' has not been created.`);
         });
     });
 });
